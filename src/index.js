ab3bde9d63a3fac9affbeaea25ab24e6836833ba
Craig Freeman <craigfreeman@gmail.com>
2020-05-10 23:52:47 -0600

    Netlify?

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config({ path: "variables.env" });
 const express = require("express");
-const serverlessHttp = require("serverless-http");
+
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
@@ -85,18 +85,5 @@ if (process.env.NODE_ENV === "development") {
   //     console.log(`Server is now running on http://localhost:${details.port}`);
   //   }
   // );
-} else {
-  module.exports.handler = serverlessHttp(app, {
-    /**
-     * **** IMPORTANT ****
-     * this request() function is important because
-     * it adds the lambda's event and context object
-     * into the express's req object so you can access
-     * inside the resolvers or routes if youre not using apollo
-     */
-    request(req, event, context) {
-      req.event = event;
-      req.context = context;
-    }
-  });
-}
+
+module.exports.app = app;
