9b035eb50c37aad8e14944c401611b08efad698d
Craig Freeman <craigfreeman@gmail.com>
2020-05-11 00:34:31 -0600

    Relative paths ugh

--- src/db.js
+++ src/db.js
@@ -1,11 +1,15 @@
 // Connect to remote Prisma DB
-const { Prisma } = require('prisma-binding');
+const { Prisma } = require("prisma-binding");
+const { importSchema } = require("graphql-import");
+const { gql } = require("apollo-server-express");
+
+const prismaTypeDefs = importSchema(`${__dirname}/generated/prisma.graphql`);
 
 const db = new Prisma({
-  typeDefs: 'src/generated/prisma.graphql',
+  typeDefs: prismaTypeDefs,
   endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
-  debug: process.env.NODE_ENV === 'development',
+  debug: process.env.NODE_ENV === "development"
 });
 
 module.exports = db;
