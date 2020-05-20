2272b09bf221e8e1eadfba1c56810978e11c1778
Craig Freeman <craigfreeman@gmail.com>
2020-05-19 20:43:58 -0600

    minor

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -9,7 +9,7 @@ const config = require("../config");
 
 const Query = {
   myself(parent, args, ctx, info) {
-    console.log(ctx.req);
+    console.log("LOGIN", ctx.req.userId, ctx.req.user, ctx.req);
     // Check if there is a current user
     if (!ctx.req.userId) {
       return null;
